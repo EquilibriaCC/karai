3ca9fd7f67dc3823fc31b27eaf793b8379225c5b
thomasdparker1 <49144784+thomasdparker1@users.noreply.github.com>
2020-10-24 21:30:26 -0700

    cleanup

--- main.go
+++ main.go
@@ -7,20 +7,9 @@ import (
 
 // Hello Karai
 func main() {
-	//osCheck()
 	c := config.Config_Init()
 	flags(&c)
-	//checkDirs()
-	//createTables()
-	//cleanData()
-	//keys := initKeys()
-	//createRoot()
-	//ascii()
 	var s network.Server
 	go network.Protocol_Init(&c, &s)
-	//go getDataCovid19(1000)
-	//go getDataOgre(500)
-	//go generateRandomTransactions()
 	inputHandler(&s)
-
 }
